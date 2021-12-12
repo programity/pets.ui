294dd817a58efc08d8166c2ca2cbc6cff3cdf805
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2021-12-12 17:40:08 -0400

    add posts list and interfaces of user

---
+++ src/app/interfaces/interfaces.ts
@@ -0,0 +1,23 @@
+
+
+export interface RespuestaPosts {
+  ok: boolean;
+  pagina: number;
+  posts: Post[];
+}
+
+export interface Post {
+  _id?: string;
+  mensaje?: string;
+  imgs?: string[];
+  coords?: string;
+  user?: User;
+  created?: string;
+}
+
+export interface User {
+  _id?: string;
+  name?: string;
+  avatar?: string;
+  email?: string;
+}
--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  constructor(private postsService: PostsService) {}
+
+  ngOnInit(): void {
+      this.postsService.getPosts().subscribe( resp => {
+        console.log(resp);
+      });
+  }
 
 }
